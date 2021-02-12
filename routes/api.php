df22e870ddb780585e7c858bcc49ddd7ea6b3297
Sergey <ma@ctoh.net>
2021-02-12 10:20:04 +0300

    установка модуля jwt аутентификации

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\BookController;
 use \App\Http\Controllers\AuthorController;
+use \App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,16 @@ use \App\Http\Controllers\AuthorController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('registration', [AuthController::class, 'registration']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
